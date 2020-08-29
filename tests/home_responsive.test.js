beda235614ea856e03860f88f0f3eb19c1b58e8d
Denzel Adrian Co <denzeladrianco@Denzels-MacBook-Pro.local>
2020-08-29 22:24:55 +0800

    selecting xpath elements

--- tests/home_responsive.test.js
+++ tests/home_responsive.test.js
@@ -1,16 +1,16 @@
-// const timeout = 1000000;
+const timeout = 1000000;
 
-// describe("Test Responsiveness of Homepage", () => {
-//   test("Screenshot of all widths", async () => {
-//     for (u in URL) {
-//       await page.goto(URL[u], { waitUntil: "domcontentloaded" });
-//       await page.evaluate(() => {
-//         localStorage.setItem('isSubscribed', true);
-//       });
-//       for (w in WIDTH) {
-//         await page.setViewport({ width: WIDTH[w], height: HEIGHT });
-//         await page.screenshot({ path: `./screenshots/${u}/${w}.jpeg`, type: 'jpeg' });
-//       }
-//     }
-//   }, timeout);
-// });
+describe("Test Responsiveness of Homepage", () => {
+  test("Screenshot of all widths", async () => {
+    for (u in URL) {
+      await page.goto(URL[u], { waitUntil: "domcontentloaded" });
+      await page.evaluate(() => {
+        localStorage.setItem('isSubscribed', true);
+      });
+      for (w in WIDTH) {
+        await page.setViewport({ width: WIDTH[w], height: HEIGHT });
+        await page.screenshot({ path: `./screenshots/${u}/${w}.jpeg`, type: 'jpeg' });
+      }
+    }
+  }, timeout);
+});
--- tests/targets_buttons.test.js
+++ tests/targets_buttons.test.js
@@ -1,19 +1,14 @@
-const { TestScheduler } = require("jest")
+// const { TestScheduler } = require("jest")
 
-const timeout = 10000
+// const timeout = 10000
 
-describe('Testing Targets Page Elements', () => {
-    test("Male Issues Div Test", async () => {
-        await page.goto(URL.TARGETS, {waitUntil: "domcontentloaded"})
-        await page.evaluate(() => {
-            localStorage.setItem('isSubscribed', true);
-        })
-        const linkHandlers = await page.$x('//*[@id="app"]/nav/div[1]/ul/li[2]/a');
-
-        if (linkHandlers.length > 0) {
-            await linkHandlers[0].click();
-        } else {
-            throw new Error("Link not found");
-        }
-    }, timeout)
-})
+// describe('Testing Targets Page Elements', () => {
+//     test("Male Issues Div Test", async () => {
+//         await page.goto(URL.TARGETS, {waitUntil: "domcontentloaded"})
+//         await page.evaluate(() => {
+//             localStorage.setItem('isSubscribed', true);
+//         })
+//         const elements = await page.$x('//*[@id="app"]/nav/div[1]/ul/li[2]/a')
+//         await elements[0].click() 
+//     }, timeout)
+// })
