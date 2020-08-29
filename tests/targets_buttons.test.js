cf25bc21f0d1aa067b630fa4a1fb610b2a690628
Denzel Adrian Co <denzeladrianco@Denzels-MacBook-Pro.local>
2020-08-29 22:16:56 +0800

    xpath element selector

--- tests/targets_buttons.test.js
+++ tests/targets_buttons.test.js
@@ -8,7 +8,12 @@ describe('Testing Targets Page Elements', () => {
         await page.evaluate(() => {
             localStorage.setItem('isSubscribed', true);
         })
+        const linkHandlers = await page.$x('//*[@id="app"]/nav/div[1]/ul/li[2]/a');
 
-        
+        if (linkHandlers.length > 0) {
+            await linkHandlers[0].click();
+        } else {
+            throw new Error("Link not found");
+        }
     }, timeout)
 })
