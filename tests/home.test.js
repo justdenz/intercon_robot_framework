56f7e984fbb448ba489adfc12b9d76c3a4f43403
Denzel Adrian Co <denzeladrianco@Denzels-MacBook-Pro.local>
2020-08-31 16:48:12 +0800

    home targets modal

---
+++ tests/home.test.js
@@ -0,0 +1,40 @@
+jest.setTimeout(30000)
+
+beforeAll(async () => {
+  await page.goto(URL.HOME, { waitUntil: "networkidle0" });
+  await page.evaluate(() => {
+    localStorage.setItem('modalShown', false);
+    localStorage.setItem('isSubscribed', true);
+    localStorage.setItem('isFirstVisit', false);
+  });
+})
+
+describe("Test Home Target Modal", () => {
+    test('Test if home target has modal', async () => {
+        let exist = await page.evaluate(async () => {
+            return await new Promise(async resolve => {
+                const delay = (time) => {
+                    return new Promise(function (resolve) {
+                        setTimeout(resolve, time)
+                    });
+                }
+                let element = document.querySelector('#app > div:nth-child(6) > div > div:nth-child(1) > a > div > div')
+                test = true
+                element.click()
+                await delay(500)
+                let body = document.querySelector('#modal-scrollable___BV_modal_body_')
+                test = test && !!body
+                await delay(500)
+                resolve(test)
+            })
+        })
+        expect(exist).toBe(true)
+    })
+});
+  
+  
+const delay = (time) => {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, time)
+    });
+}
